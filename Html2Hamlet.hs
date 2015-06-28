378d12576b6bfae5df628eb3102201700c9ba867
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2015-06-28 21:36:19 +0900

    refactoring: use html-conduit instead of xmlhtml

--- Html2Hamlet.hs
+++ Html2Hamlet.hs
@@ -6,24 +6,23 @@ module Main (main) where
 
 import           Blaze.ByteString.Builder
 import           Blaze.ByteString.Builder.Char.Utf8
-import           Control.Applicative
+import           Control.Arrow
 import           Control.DeepSeq
 import           Control.Exception
 import           Control.Monad
 import           Control.Monad.Trans
-import qualified Data.ByteString.Char8              as S
 import qualified Data.ByteString.Lazy.Char8         as L
 import           Data.Char
 import           Data.List
-import           Data.Maybe
+import qualified Data.Map                           as Map
 import           Data.Monoid
 import qualified Data.Text                          as T
 import           Data.Version                       (showVersion)
-import           Network
 import           Network.HTTP.Conduit
 import           Options.Declarative
-import           Text.XmlHtml
-import           Text.XmlHtml.Cursor
+import qualified Text.HTML.DOM                      as HTML
+import           Text.XML
+import           Text.XML.Cursor
 
 import           Paths_html2hamlet                  (version)
 
@@ -33,20 +32,17 @@ main = run "html2hamlet" (Just $ showVersion version) cmd
 cmd :: Arg "FILES/URLS..." [String]
     -> Cmd "HTML to Hamlet converter" ()
 cmd (get -> []) = liftIO $
-  writeHamlet "stdin" S.getContents S.putStr
-
+  writeHamlet L.getContents L.putStr
 cmd (get -> files) = liftIO $ do
   forM_ files $ \file -> do
     if any (`isPrefixOf` file) ["http://", "https://"]
-      then do
-      writeHamlet file (L.toStrict <$> simpleHttp file) $ S.writeFile (httpFileName file)
-      else do
-      writeHamlet file (S.readFile file) $ S.writeFile (changeSuffix file)
+      then writeHamlet (simpleHttp file) $ L.writeFile (httpFileName file)
+      else writeHamlet (L.readFile file) $ L.writeFile (changeSuffix file)
 
-writeHamlet :: String -> IO S.ByteString -> (S.ByteString -> IO ()) -> IO ()
-writeHamlet sourceName reader writer = do
+writeHamlet :: IO L.ByteString -> (L.ByteString -> IO ()) -> IO ()
+writeHamlet reader writer = do
   con <- reader
-  let dest = convert sourceName con
+  let dest = convert con
   evaluate $ rnf dest
   writer dest
 
@@ -55,53 +51,51 @@ httpFileName url = changeSuffix nsuf
   where
     nsuf | null suf = "index.html"
          | otherwise = suf
-    suf = dropQuery $ dropFrag $ reverse $ takeWhile (/='/') $ reverse url
-    dropFrag = takeWhile (/='#')
-    dropQuery = takeWhile (/='?')
+    suf = dropQuery $ dropFrag $ reverse $ takeWhile (/= '/') $ reverse url
+    dropFrag  = takeWhile (/= '#')
+    dropQuery = takeWhile (/= '?')
 
 changeSuffix :: String -> String
 changeSuffix file
   | any (`isSuffixOf` file) [".html", ".htm"] =
-    (++"hamlet") $ reverse $ dropWhile (/='.') $ reverse file
+    (++ "hamlet") $ reverse $ dropWhile (/= '.') $ reverse file
   | otherwise =
     file ++ ".hamlet"
 
-convert :: String -> S.ByteString -> S.ByteString
-convert fname content = toByteString $ cvt $ fromNodes nodes
+convert :: L.ByteString -> L.ByteString
+convert = toLazyByteString . cvt . fromDocument . HTML.parseLBS
   where
-    Right (HtmlDocument enc typ nodes) = parseHTML fname content
-
     cvt = (fromString "!!!" <>) .
           (<> fromString "\n") .
           go 0
 
-    go lev (Just cur) = slf <> cld <> bro
-      where
-        slf = single lev (current cur)
-        cld = go (lev+1) (firstChild cur)
-        bro = go lev (right cur)
+    go lev cur =
+      single lev (node cur) <>
+      mconcat (map (go $ lev + 1) $ child cur)
 
-    go lev Nothing =
-      mempty
+    single lev (NodeElement (Element tag attrs _)) =
+      newline lev <>
+      fromString "<" <>
+      fromText (nameLocalName tag) <>
+      battr (map (first nameLocalName) $ Map.toList attrs) <>
+      fromString ">"
 
-    single lev (TextNode txt)
+    single lev (NodeContent txt)
       | T.all isSpace txt = mempty
       | otherwise =
         newline lev <>
         fromText (sanitize txt)
-    single lev (Comment comment) =
+
+    single lev (NodeComment comment) =
       mconcat $ do
         line <- T.lines comment
         return $
           newline lev <>
           fromString "$# " <>
           fromText line
-    single lev (Element tag attrs _) =
-      newline lev <>
-      fromString "<" <>
-      fromText tag <>
-      battr attrs <>
-      fromString ">"
+
+    single _ (NodeInstruction _) =
+      mempty
 
     newline lev =
       fromString "\n" <>
@@ -125,4 +119,4 @@ convert fname content = toByteString $ cvt $ fromNodes nodes
         fromString "\""
 
     sanitize = T.dropWhile isSpace .
-               T.map (\c -> if c=='\n' then ' ' else c)
+               T.map (\c -> if c == '\n' then ' ' else c)
