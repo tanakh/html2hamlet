f1c97840c5ee96ba8912840d5e9a67275feb164b
Hideyuki Tanaka <tanaka.hideyuki@gmail.com>
2015-06-28 22:49:43 +0900

    Switch to wl-pprint-text

--- Html2Hamlet.hs
+++ Html2Hamlet.hs
@@ -4,27 +4,26 @@
 
 module Main (main) where
 
-import           Blaze.ByteString.Builder
-import           Blaze.ByteString.Builder.Char.Utf8
 import           Control.Arrow
-import           Control.DeepSeq
-import           Control.Exception
 import           Control.Monad
 import           Control.Monad.Trans
-import qualified Data.ByteString.Lazy.Char8         as L
+import qualified Data.ByteString.Lazy.Char8   as L
 import           Data.Char
 import           Data.List
-import qualified Data.Map                           as Map
+import qualified Data.Map                     as Map
 import           Data.Monoid
-import qualified Data.Text                          as T
-import           Data.Version                       (showVersion)
+import qualified Data.Text                    as T
+import qualified Data.Text.Lazy               as TL
+import           Data.Version                 (showVersion)
 import           Network.HTTP.Conduit
 import           Options.Declarative
-import qualified Text.HTML.DOM                      as HTML
+import           System.IO
+import qualified Text.HTML.DOM                as HTML
+import           Text.PrettyPrint.Leijen.Text hiding ((<>))
 import           Text.XML
-import           Text.XML.Cursor
+import           Text.XML.Cursor              (child, fromDocument, node)
 
-import           Paths_html2hamlet                  (version)
+import           Paths_html2hamlet            (version)
 
 main :: IO ()
 main = run "html2hamlet" (Just $ showVersion version) cmd
@@ -32,19 +31,16 @@ main = run "html2hamlet" (Just $ showVersion version) cmd
 cmd :: Arg "FILES/URLS..." [String]
     -> Cmd "HTML to Hamlet converter" ()
 cmd (get -> []) = liftIO $
-  writeHamlet L.getContents L.putStr
+  writeHamlet L.getContents putDoc
 cmd (get -> files) = liftIO $ do
   forM_ files $ \file -> do
     if any (`isPrefixOf` file) ["http://", "https://"]
-      then writeHamlet (simpleHttp file) $ L.writeFile (httpFileName file)
-      else writeHamlet (L.readFile file) $ L.writeFile (changeSuffix file)
+      then writeHamlet (simpleHttp file) $ \doc -> withFile (httpFileName file) WriteMode $ \h -> hPutDoc h doc
+      else writeHamlet (L.readFile file) $ \doc -> withFile (changeSuffix file) WriteMode $ \h -> hPutDoc h doc
 
-writeHamlet :: IO L.ByteString -> (L.ByteString -> IO ()) -> IO ()
-writeHamlet reader writer = do
-  con <- reader
-  let dest = convert con
-  evaluate $ rnf dest
-  writer dest
+writeHamlet :: IO L.ByteString -> (Doc -> IO ()) -> IO ()
+writeHamlet reader writer =
+  writer . convert =<< reader
 
 httpFileName :: String -> String
 httpFileName url = changeSuffix nsuf
@@ -62,61 +58,25 @@ changeSuffix file
   | otherwise =
     file ++ ".hamlet"
 
-convert :: L.ByteString -> L.ByteString
-convert = toLazyByteString . cvt . fromDocument . HTML.parseLBS
-  where
-    cvt = (fromString "!!!" <>) .
-          (<> fromString "\n") .
-          go 0
+convert :: L.ByteString -> Doc
+convert = cvt . fromDocument . HTML.parseLBS where
+  cvt doc = "$doctype 5" <$$> go doc
+  go cur = fromNode (node cur) <$$> indent 4 (vsep (map go $ child cur))
 
-    go lev cur =
-      single lev (node cur) <>
-      mconcat (map (go $ lev + 1) $ child cur)
+fromNode :: Node -> Doc
+fromNode (NodeElement (Element tag attrs _)) =
+  "<" <> hsep (text' (nameLocalName tag): battr attrs) <> ">"
+fromNode (NodeContent t    )
+  | T.all isSpace t = mempty
+  | otherwise       = string $ TL.fromStrict $ T.dropWhile isSpace t
+fromNode (NodeComment t    ) = vsep $ map (("$# " <>) . text') $ T.lines t
+fromNode (NodeInstruction _) = mempty
 
-    single lev (NodeElement (Element tag attrs _)) =
-      newline lev <>
-      fromString "<" <>
-      fromText (nameLocalName tag) <>
-      battr (map (first nameLocalName) $ Map.toList attrs) <>
-      fromString ">"
+battr :: Map.Map Name T.Text -> [Doc]
+battr = concatMap (f . first nameLocalName) . Map.toList where
+  f ("class", val) = map (("." <>) . text') $ T.words val
+  f ("id",    val) = ["#" <> text' val]
+  f (key,     val) = [text' key <> "=\"" <> text' val <> "\""]
 
-    single lev (NodeContent txt)
-      | T.all isSpace txt = mempty
-      | otherwise =
-        newline lev <>
-        fromText (sanitize txt)
-
-    single lev (NodeComment comment) =
-      mconcat $ do
-        line <- T.lines comment
-        return $
-          newline lev <>
-          fromString "$# " <>
-          fromText line
-
-    single _ (NodeInstruction _) =
-      mempty
-
-    newline lev =
-      fromString "\n" <>
-      fromString (replicate (lev*2) ' ')
-
-    battr attrs = mconcat $ map f attrs where
-      f ("id", val) =
-        fromString " #" <>
-        fromText val
-      f ("class", val) =
-        mconcat $ do
-          klass <- T.words val
-          return $
-            fromString " ." <>
-            fromText klass
-      f (key, val) =
-        fromString " " <>
-        fromText key <>
-        fromString "=\"" <>
-        fromText val <>
-        fromString "\""
-
-    sanitize = T.dropWhile isSpace .
-               T.map (\c -> if c == '\n' then ' ' else c)
+text' :: T.Text -> Doc
+text' = text . TL.fromStrict
